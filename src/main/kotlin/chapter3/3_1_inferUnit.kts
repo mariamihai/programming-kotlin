91be71afe784ff0e14f66edc8da0bf68273d236d
mariamihai <maria.ic.mihai@gmail.com>
2021-06-12 16:34:26 +0200

    Chapter 3 code (#4)
    * Renamed chapter 1 and 2 files
    
    * Cchapter 3 - initial part
    
    * Chapter 3 - complete code.
    
    * Updated README after Chapter 3.

---
+++ src/main/kotlin/chapter3/3_1_inferUnit.kts
@@ -0,0 +1,7 @@
+// Kotlin will infer the type as Unit if the function
+// isn’t returning anything
+fun sayHello() = println("Well, hello")
+
+// ERROR
+// val message: String = sayHello()
+// type mismatch: inferred type is Unit but String was expected
