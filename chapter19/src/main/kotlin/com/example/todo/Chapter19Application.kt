31028e4f25dbe704199b2d18cd9d465313f264b2
mariamihai <maria.ic.mihai@gmail.com>
2021-07-30 13:34:40 +0200

    Chapter 19 - initial Spring Boot project for the chapter

---
+++ chapter19/src/main/kotlin/com/example/todo/Chapter19Application.kt
@@ -0,0 +1,11 @@
+package com.example.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter19Application
+
+fun main(args: Array<String>) {
+    runApplication<Chapter19Application>(*args)
+}
