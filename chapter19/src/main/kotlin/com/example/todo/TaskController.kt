7d8762ee6b7e65a8b49ee95308969a21bf64e8d8
mariamihai <maria.ic.mihai@gmail.com>
2021-07-30 13:50:04 +0200

    Chapter 19 - Creating controller and verifying the endpoint
    curl -w "\n" http://localhost:8080/task
    to be implemented

---
+++ chapter19/src/main/kotlin/com/example/todo/TaskController.kt
@@ -0,0 +1,13 @@
+package com.example.todo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/task")
+class TaskController {
+
+    @GetMapping
+    fun tasks() = "to be implemented"
+}
